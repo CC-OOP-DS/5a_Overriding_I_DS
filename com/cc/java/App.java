ca1f8bf25043c35d952c74b557800b89beefd2a3
Daniel <schindlerdaniel@web.de>
2022-11-28 10:05:33 +0100

    child parent override

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,13 @@ public class App {
     
     public static void main(String[] args) {
      
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello(""));
+
+        Parent parent = new Parent();
+        output(parent.sayHello());
+        output(parent.sayHello(""));
     }
 
     public static void output(String outputStr) {
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Parent {
+    
+   public String sayHello(String name) {
+    
+        return "Hello with string from parent";
+   }
+   
+    public String sayHello() {
+
+        return "Hello from parent";
+    }
+}
