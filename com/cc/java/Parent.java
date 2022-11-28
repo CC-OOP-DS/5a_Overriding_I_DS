a28150faf2a92a44ad5cdad455f7bcd6b9c1cc06
Daniel <schindlerdaniel@web.de>
2022-11-28 10:42:19 +0100

    grandparent

--- com/cc/java/Parent.java
+++ com/cc/java/Parent.java
@@ -1,14 +1,14 @@
 package com.cc.java;
 
-public class Parent {
+public class Parent extends GrandParent {
     
    public String sayHello(String name) {
     
         return "Hello with string from parent";
    }
    
-    public String sayHello() {
+    // public String sayHello() {
 
-        return "Hello from parent";
-    }
+    //     return "Hello from parent";
+    // }
 }
